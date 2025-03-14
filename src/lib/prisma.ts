630611ef1447c77c626b3ceb73c67009541a76bf
Dev Nome <numanb@code-x.co>
2025-03-14 15:42:03 +0500

    compleate prisma connection and clerck connect to neno with user action

---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
+
+const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+
+// const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
